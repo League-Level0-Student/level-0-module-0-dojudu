7574a59728099a8a933903731a685b1ae9919fd4
ilabstaff <ilabstaff@172.16.116.77>
2019-02-13 17:53:22 -0800

    my first job of program

--- src/strings_and_dialogs/MyJavaProgram.java
+++ src/strings_and_dialogs/MyJavaProgram.java
@@ -7,8 +7,21 @@ public class MyJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+		Robot john = new Robot();
+   john.setSpeed(100);
+   john.setPenColor(Color .red);
+   john.penDown();
+   
+   john.move(200);
+   john.turn(90);
+   john.move(200);
+   john.turn(90);
+   john.move(200);
+   john.turn(90);
+   john.move(200);
+   john.turn(90);
+   john.penUp();
 	
-		
-		
+	
 	}
 }
